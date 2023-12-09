d82e74eb9e300532b422921312b25cf308e1d522
183akshay <sharmahoney398@gmail.com>
2023-12-09 20:39:19 +0530

    tut-3

--- Learning/src/main.ts
+++ Learning/src/main.ts
@@ -49,3 +49,141 @@ const sumOfNum = (num1: number | string, num2: number | string): number | string
 
 // ------------------------------------------------------
 
+/* Arrays Tuples and Objects */
+
+
+// Tuple has strict data type for specified size, after that if tuple is not read only
+// than there is no type security for pushed elements for tuple. 
+let ourReadonlyTuple: readonly [number, boolean, string] = [5, true, 'The Real Coding God'];
+/* make tuple readonly as without it there will be no stype safety for push elements */
+
+let ourTuple: [number, boolean, string] = [5, true, 'The Real Coding God'];
+
+// Arrays
+let arr = [1,true,'akshay']
+
+/* 
+    *imp points
+    arr = ourTuple -> correct
+    ourTuple = arr -> wrong
+*/
+
+/* Objects */
+// https://www.w3schools.com/typescript/typescript_object_types.php
+
+console.log('typeof(sumOfNum) :>> ', typeof(sumOfNum));
+// remember tho a function is object, but type of return it as function only.
+
+const car: {
+    wheels: number,
+    engine: string
+} = {
+    wheels: 4,
+    engine: 'maruti'
+}
+
+// error on adding non existing variable => car.name = 'hyundia'
+type WheelCount = number
+type Truck = {
+    wheels: WheelCount,
+    engine: string
+}
+
+interface TruckInterface {
+    wheels: WheelCount,
+    engine: string
+}
+
+// alias type vs interface => interface only used with objects.
+
+let truck_1: Truck = {
+    wheels: 4,
+    engine: 'toyota'
+}
+
+let truck_2 = {
+    wheels: 8,
+    engine: 'Tata',
+    color: 'red'
+}
+
+/* Destructuring creates new object, remeber assignment
+   will take place, but since it is converted to js file
+   type safety gets messed after. so color also gets in 
+   truck_1 object even tho it is Truck type. So this is
+   one of mechanism to explicilty only get required 
+   paramters.
+ 
+let {color, ...newTruck} = truck_2
+truck_1 = newTruck
+truck_1.wheels = 9
+
+*/
+
+truck_1 = truck_2 // color also get into prod, this fk prod as it compiles to js.
+
+// here truck_1 logs 
+console.log('truck1 :>> ', truck_1);
+
+
+/* Optional parameter in Objects */
+interface Guitarist {
+    genre? : string
+    songsCount: number
+}
+
+let arijitSingh: Guitarist = {
+    songsCount: 4,
+    genre: 'sad',
+}
+
+let atifAslam = {
+    songsCount: 3,
+    genre: 'melody',
+}
+
+/* 
+    atifAslam = arijitSingh => gives error as undefined is not assignable to string
+*/
+arijitSingh = atifAslam
+console.log('arijitSingh :>> ', arijitSingh);
+
+/* 
+
+    Error=> because firstly its a bad practice to use methods
+    over optional field as it can be null/undefined
+    const greet = (guitarist: Guitarist) {
+        return guitarist.genre.toUpperCase;
+    } 
+
+    instead
+*/
+const greet = (guitarist: Guitarist) => {
+
+    if(guitarist.genre) {
+        return `Genre is ${guitarist.genre}`;
+    }
+
+    return `genre data is absent`;
+} 
+
+console.log('greet(arijitSingh) :>> ', greet(arijitSingh));
+
+/* Enum =>
+    Unlike other stuff in TS which are just extension or typed property of JS
+    Enum are actually feature extension in TS.
+*/
+
+/* normally first elements starts from 0, but we can change ordering */
+enum Grade{
+    U=1,
+    D,
+    C,
+    B,
+    A,
+}
+
+console.log(Grade.B);
+
+const emillaMathGrade: Grade = Grade.U
+// console.log(emillaMathGrade==Grade.U);
