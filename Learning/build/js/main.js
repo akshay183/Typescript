09e51ca5a546f0d9bd1619fc67d1f16dee9e4467
183akshay <sharmahoney398@gmail.com>
2023-12-09 16:02:21 +0530

    Learning Typescript tut-1

---
+++ Learning/build/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("hello");
+let a = 1;
+let b = 2;
+let c = a * b;
+console.log('c :>> ', c);
+// ------------------------------------------------------
---
+++ Learning/src/main.ts
@@ -0,0 +1,13 @@
+console.log("hello")
+
+let a:number = 1
+let b:number = 2
+
+let c = a*b
+console.log('c :>> ', c);
+
+// ------------------------------------------------------
+
+
+
+
